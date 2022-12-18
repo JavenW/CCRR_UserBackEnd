cbe5da748c7183b63d27e5740df30f2a2d79c5cb
JavenW <513203628@qq.com>
2022-12-18 10:56:04 -0500

    add middleware for user

--- user_backend/app.py
+++ user_backend/app.py
@@ -1,29 +1,39 @@
-
 from flask import Flask, Response, request, redirect, url_for
-from datetime import datetime
 import json
 from user_database import UserResource
 from flask_cors import CORS
-import boto3
+import os
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+from user import User
+
+
+# Configuration
+GOOGLE_CLIENT_ID = "35761253348-sek252rrnujnijbah5hta587c0cr1b6m.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
 
 # Create the Flask application object.
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 
-# @app.route("/users", methods=["GET"])
-# def get_all_users():
-#     result = UserResource.get_users()
-#     if result:
-#         rsp = Response(json.dumps(result), status=200, content_type="app.json")
-#     else:
-#         rsp = Response("NOT FOUND", status=404, content_type="text/plain")
-#     return rsp
-
-@app.route("/getallergy/<email>", methods=["GET"])
-def get_allergy(email):
-    result = UserResource.get_user_allergy_by_email(email)
+@app.route("/getallergy", methods=["GET"])
+def get_allergy():
+    userid = request.args.get('userid', None)
+    token = request.args.get('token', None)
+    res = User.checkAuthToken(userid, token)
+    if not res:
+        return Response(json.dumps({}), status=403, content_type="app.json")
+    result = UserResource.get_user_allergy_by_user_id(userid)
     if result:
         rsp = Response(json.dumps(result), status=200, content_type="app.json")
     else:
@@ -32,57 +42,137 @@ def get_allergy(email):
 
 @app.route("/addallergy", methods=["post"])
 def add_allergy():
-    email = request.form['email']
+    userid = request.form['userid']
+    token = request.form['token']
+    res = User.checkAuthToken(userid, token)
+    if not res:
+        return Response(json.dumps({}), status=403, content_type="app.json")
     allergy = request.form['allergy']
-    UserResource.add_allergy_by_email(email, allergy)
+    User.add_allergy_by_user_id(userid, allergy)
 
     return Response(json.dumps({}), status=200, content_type="app.json")
 
 @app.route("/deleteallergy", methods=["post"])
 def delete_allergy():
-    email = request.form['email']
+    userid = request.form['userid']
+    token = request.form['token']
+    res = User.checkAuthToken(userid, token)
+    if not res:
+        return Response(json.dumps({}), status=403, content_type="app.json")
     allergy = request.form['allergy']
-    UserResource.delete_allergy_by_email(email, allergy)
+    User.delete_allergy_by_user_id(userid, allergy)
 
     return Response(json.dumps({}), status=200, content_type="app.json")
 
 
-@app.route("/login", methods=["POST"])
-def add_user():
-    email = request.form['email']
-    name  = request.form['name']
 
-    client = boto3.client('ses')
-    res = client.list_identities(
-        IdentityType='EmailAddress'
+@app.route("/checklogin", methods=["POST"])
+def checklogin():
+    print("here1")
+    id = request.form['userid']
+    print("here2")
+    token = request.form['authtoken']
+    print("here3")
+    res = User.checkAuthToken(id, token)
+    if res:
+        return Response(json.dumps({}), status=200, content_type="app.json")
+    else:
+        return Response(json.dumps({}), status=403, content_type="app.json")
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
     )
-    identities = res['Identities']
-    if email not in identities:
-        client.verify_email_identity(
-            EmailAddress=email
-        )
-    
-    user_id = UserResource.get_userid_by_email(email)
-    if user_id == -1:
-        UserResource.add_user_by_info(email, name)
+    # return redirect(request_uri)
+    response = redirect(request_uri)
+    response.headers = {"Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Headers': "*",
+        'Access-Control-Allow-Methods': "*"}
+    return response
 
 
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    auth_token = User.encode_auth_token(unique_id, app.config.get('SECRET_KEY'))
+
+    # Doesn't exist? Add to database
+    if not User.checkUser(unique_id):
+        User.create(unique_id, users_name, users_email, picture, auth_token)
+    else:
+        # Begin user session by logging the user in
+        User.update_authtoken(unique_id, auth_token)
+
+    # Send user back to homepage
+    return redirect("https://localhost:3000/loginComplete?userid="+unique_id+"&name="+users_name+"&email="+users_email+"&pict="+picture+"&authtoken="+auth_token)
+
+
+
+@app.route("/logout")
+def logout():
+    userid = request.form['userid']
+    User.logout(userid)
     return Response(json.dumps({}), status=200, content_type="app.json")
 
 
-#@app.route("/l", methods=["DELETE"])
-# def delete_user(email):
-    #email = request.args.get('email')
-    # UserResource.delete_user_by_email(email)
 
-
-#@app.route("/login", methods=["POST"])
-# def update_phone(email, phone):
-#     UserResource.update_phone_by_email_and_phone(email, phone)
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", port=5011)
+    app.run(ssl_context="adhoc")
+    # app.run(host="0.0.0.0", port=5000)
 
 
---
+++ user_backend/user.py
@@ -0,0 +1,148 @@
+import pymysql
+import datetime
+import jwt
+
+class User():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def encode_auth_token(user_id, secret_key):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                # app.config.get('SECRET_KEY'),
+                secret_key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def update_authtoken(user_id, token):
+        sql = "UPDATE user_profile.user_info SET auth_token = %s WHERE (user_id = %s);"
+        data = (token, user_id)
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        conn.commit() 
+
+    @staticmethod
+    def checkUser(user_id):
+        sql = "select user_id,email,name,profile_pic from user_profile.user_info where user_id = %s;"
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=user_id)
+        result = cur.fetchone()
+        if result == None:
+            return None
+
+        return True
+
+    @staticmethod
+    def checkAuthToken(user_id, token):
+        sql = "select * from user_profile.user_info where user_id = %s and auth_token = %s;"
+        conn = User._get_connection()
+        data = (user_id, token)
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        result = cur.fetchone()
+        if result == None:
+            return False
+        return True
+    
+    @staticmethod
+    def logout(user_id):
+        sql = "UPDATE user_profile.user_info SET auth_token = '1' WHERE (user_id = %s);"
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=user_id)
+        conn.commit()
+
+    @staticmethod
+    def create(id_, name, email, profile_pic, token):
+        sql = "insert into user_profile.user_info(user_id, email, name, profile_pic, auth_token) values (%s, %s, %s, %s, %s)"
+        data = (id_, email, name, profile_pic, token)
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        conn.commit() 
+
+    @staticmethod
+    def _get_connection():
+
+        # usr = os.environ.get("DBUSER")
+        # pw = os.environ.get("DBPW")
+        # h = os.environ.get("DBHOST")
+        usr = "admin"
+        pw = "dbuserdbuser"
+        h = "userdb.ci9bmsfj6m9q.us-east-1.rds.amazonaws.com"
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_userid_by_email(email):
+        sql = "select user_id from user_profile.user_info where email = %s";
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=email)
+        result = cur.fetchone()
+        if result == None:
+            return -1
+        return result['user_id']
+
+
+    @staticmethod
+    def get_user_allergy_by_id(user_id):
+        sql = "SELECT allergy FROM user_profile.user_allergy where user_id = %s";
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=user_id)
+        result = cur.fetchall()
+        allergies = []
+        for pair in result:
+            allergies.append(pair['allergy'])
+        return allergies
+
+    @staticmethod
+    def add_user_by_info(email, name):
+        sql = "insert into user_profile.user_info(email, name) values (%s, %s)"
+        data = (email, name)
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        conn.commit() 
+
+    @staticmethod
+    def add_allergy_by_user_id(user_id, allergy):
+        sql = "insert into user_profile.user_allergy(user_id, allergy) values (%s, %s)"
+        data = (user_id, allergy)
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        conn.commit()
+
+    @staticmethod
+    def delete_allergy_by_user_id(user_id, allergy):
+        sql = "delete from user_profile.user_allergy where user_id = %s and allergy = %s"
+        data = (user_id, allergy)
+        conn = User._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, data)
+        conn.commit()
